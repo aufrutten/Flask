a3a0e8896ebba441dd2e5db3fd9c6c45c0de73cd
Semykopenko Ihor <aufrutten@gmail.com>
2022-11-12 12:50:25 +0100

    adding path_to_folder in app.config

--- app_main.py
+++ app_main.py
@@ -7,12 +7,12 @@ def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        instance_relative_config=True,
+        instance_relative_config=True
     )
+    app.config['path_to_folder'] = str(pathlib.PosixPath(__file__).parent / 'tests' / 'test_reportMonaco' / 'data')
     return app
 
 
-path_to_folder = pathlib.PosixPath(__file__).parent / 'tests' / 'test_reportMonaco' / 'data'
 app = create_app()
 
 
@@ -20,16 +20,16 @@ app = create_app()
 @app.route('/')
 def parent_route():
     reverse_mode = True if request.values.get('order') else False
-    data = report.print_report(str(path_to_folder), html=True, reverse=reverse_mode)
+    data = report.print_report(app.config.get('path_to_folder'), html=True, reverse=reverse_mode)
     return render_template('index.html', content=data)
 
 
 @app.route('/report/drivers/')
 def drivers():
-    data = report.get_drivers(str(path_to_folder))
+    data = report.get_drivers(app.config.get('path_to_folder'))
     abbr = request.values.get('driver')
     reverse_mode = True if request.values.get('order') else False
-    list_of_drivers = report.get_drivers(path_to_folder)
+    list_of_drivers = report.get_drivers(app.config.get('path_to_folder'))
 
     if reverse_mode:
         data.reverse()
@@ -37,7 +37,7 @@ def drivers():
     if abbr:
         for _, driver, abbr_of_driver in list_of_drivers:
             if abbr == abbr_of_driver:
-                content = report.find_driver(driver, path_to_folder)[0]
+                content = report.find_driver(driver, app.config.get('path_to_folder'))[0]
                 return render_template('drivers.html', driver=content)
     else:
         return render_template('drivers.html', drivers=data)
